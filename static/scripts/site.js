63d7add562539d1adc7b5b74e1e00c9b3ec64db7
Nick <korn9509@gmail.com>
2020-09-01 20:48:09 +0300

    layout

---
+++ static/scripts/site.js
@@ -0,0 +1,8 @@
+
+$( document ).ready(function() {
+
+    console.log("READY");
+    $("#logo").delay(500).animate({ opacity: 1 }, 500)
+    $("#dropdown").delay(500).animate({ opacity: 1 }, 500)
+});
+
