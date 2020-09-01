201d8e835beecd4a2d5913fa4cb484864a392115
Nick <korn9509@gmail.com>
2020-09-01 23:58:28 +0300

    scroll fix

--- static/scripts/site.js
+++ static/scripts/site.js
@@ -13,7 +13,7 @@ $( document ).ready(function() {
         $('.fade').each(function (i) {
 
             //var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_object = $(this).position().top +$(this).outerHeight()/3;
+            var bottom_of_object = $(this).position().top +50;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
